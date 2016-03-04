cd2bf111bf974f57c79c7b40c52ef66050352555
UPIIDK <icontlac1000@alumno.ipn.mx>
2016-03-04 13:53:10 -0600

    Create ejemploherencia1.js

---
+++ rv/Source/ejemploherencia1.js
@@ -0,0 +1,44 @@
+function Pierna() {
+    //Se crea un objeto tipo 3D, que se crea con un constructor
+    THREE.Object3D.call(this);
+    this.pierna = new THREE.Mesh( new THREE.BoxGeometry(1, 5, 1));
+    this.pie = new THREE.Mesh( new THREE.BoxGeometry(2,1,1));
+    
+    this.pierna.position.y=-2.5;
+    this.pie.position.y= - 4.5;
+    this.pie.position.x=1;
+    
+    this.add(this.pierna);
+    this.add(this.pie);
+    }
+    //se usa el Constructor Prototype
+    Pierna.prototype = new THREE.Object3D();
+    
+    function setup() {
+      var cuerpo = new THREE.Mesh( newTHREE.CylinderGeometry(1,2,5,10));
+      piernaD=new Pierna();
+      piernaI=new Pierna();
+      
+      cuerpo.position.y=2;
+      piernaD.position.z=-1;
+      piernaI.position.z=1;
+      step=.01;
+      
+      escena= new THREE.Scene();
+      escena.add(cuerpo);
+      escena.add(piernaD);
+      escena.add(piernaI);
+      
+      camara = new THREE.PerspectiveCamera();
+      camara.position.z=20;
+      
+      renderer=new THREE.WebGLRenderer();
+      renderer.setSize(window.innerHeight*.95, window.innerHeight*.95);
+      document.body.appendDChild(renderer.domElement);
+      
+      function loop(){
+      requestAnimationFrame(loop);
+      renderer.render(escena, camara);
+      
+      if(Math.abs(piernaD.rotation.z) > .5)
+      
