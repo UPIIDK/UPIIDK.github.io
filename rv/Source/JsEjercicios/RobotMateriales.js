5ccece2f41dafead752b3f7fef1f0ac7a0edaeb7
UPIIDK <icontlac1000@alumno.ipn.mx>
2016-04-02 03:00:03 -0600

    Update RobotMateriales.js

--- rv/Source/JsEjercicios/RobotMateriales.js
+++ rv/Source/JsEjercicios/RobotMateriales.js
@@ -19,13 +19,13 @@ function posicionLlanta(cosa,xp,yp,zp){
 function Cuerpo(){
  THREE.Object3D.call(this);
  this.placa=new THREE.Mesh(new THREE.BoxGeometry(4,8,6),new THREE.MeshPhongMaterial({color:0x7A5100})); //Color CÁFE 
- this.caja=new THREE.Mesh(new THREE.BoxGeometry(3,2,6),new THREE.MeshPhongMaterial({color:0x3332FE})); //Color Azul
- this.cam=new THREE.Mesh(new THREE.SphereGeometry(3),new THREE.MeshLambertMaterial({color:0xff97A7})); //Color Rosa
+ this.caja=new THREE.Mesh(new THREE.BoxGeometry(5,3,8),new THREE.MeshPhongMaterial({color:0x3332FE})); //Color Azul
+ this.cam=new THREE.Mesh(new THREE.SphereGeometry(4),new THREE.MeshLambertMaterial({color:0xff97A7})); //Color Rosa
  var rotcam= new THREE.Matrix4().makeRotationZ(Math.PI/2);
  this.placa.position.y=1;
- this.caja.position.y=8.1;
+ this.caja.position.y=9;
  this.cam.applyMatrix(rotcam);
- this.cam.position.y=10;
+ this.cam.position.y=14;
  this.add(this.placa);
  this.add(this.caja);
  this.add(this.cam);
@@ -63,10 +63,10 @@ function setup(){
  escena=new THREE.Scene();
  escena.add(rob,luzPuntual);
  camara=new THREE.PerspectiveCamera();
- camara.position.z=-300;
- camara.position.y=10;
+ camara.position.z=300;
+ camara.position.y=15;
  renderer=new THREE.WebGLRenderer();
- renderer.setSize(window.innerHeight,window.innerHeight);
+ renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
  document.body.appendChild(renderer.domElement);
 }
 
