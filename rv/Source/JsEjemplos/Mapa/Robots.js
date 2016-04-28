8295bf7718f4e71671ff695915d1d19666fcab63
UPIIDK <icontlac1000@alumno.ipn.mx>
2016-04-28 17:43:42 -0500

    Update Robots.js

--- rv/Source/JsEjemplos/Mapa/Robots.js
+++ rv/Source/JsEjemplos/Mapa/Robots.js
@@ -15,7 +15,7 @@ function Robot(size,x,y) {
 
 Robot.prototype = new Agent();
 
-Robot.prototype.sense=function(enviroment){
+Robot.prototype.sense=function(environment){
 	this.sensor.set(this.position,new THREE.Vector3(Math.cos(this.rotation.z),Math.sin(this.rotation.z),0));
 		var obstaculo=this.sensor.interObjects(environment.children,true);
 		
