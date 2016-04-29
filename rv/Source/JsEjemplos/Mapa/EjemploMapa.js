a796f7aa9787456f097e0db3ea8c2b41ff871e04
UPIIDK <icontlac1000@alumno.ipn.mx>
2016-04-28 19:52:49 -0500

    Update EjemploMapa.js

--- rv/Source/JsEjemplos/Mapa/EjemploMapa.js
+++ rv/Source/JsEjemplos/Mapa/EjemploMapa.js
@@ -4,24 +4,16 @@ function Wall(size,x,y){
   this.position.x=x;
   this.position.y=y;
 }
+
 Wall.prototype=new THREE.Mesh();
-function Wall2(size,x,y){
-  THREE.Mesh.call(this,new THREE.BoxGeometry(size,size,size),new THREE.MeshPhongMaterial());
-  this.size=size;
-  this.position.x=x;
-  this.position.y=y;
-}
-Wall2.prototype=new THREE.Mesh();
+
 Environment.prototype.setMap=function(map){
   var _offset=Math.floor(map.length/2);
   for(var i=0;i<map.length;i++)
   for(var j=0;j<map.length;j++){
     if(map[i][j] === "x")
-      if (this.sensor.colision==true)
       this.add(new Wall2(1,j-_offset,-(i-_offset)));
-      else
-    this.add(new Wall(1,j-_offset,-(i-_offset)));
-    else if(map[i][j] === "r")
+        else if(map[i][j] === "r")
     this.add(new Robot(0.5,j-_offset,-(i-_offset)));
   }
 }
