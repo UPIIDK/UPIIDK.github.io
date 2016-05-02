a8fc0454dd7e9583ca4ab9e7b208969bd549b829
UPIIDK <icontlac1000@alumno.ipn.mx>
2016-05-02 00:54:11 -0500

    Update SpotLightRayCaster.js

--- rv/Source/JsEjercicios/SpotLightRayCaster.js
+++ rv/Source/JsEjercicios/SpotLightRayCaster.js
@@ -51,29 +51,24 @@ cubo3 = new THREE.Mesh( new THREE.BoxGeometry(1,1,1), new THREE.MeshPhongMateria
 cubo3.position.x=4;
 escena2.add(cubo3);
 step2=-step2;
-a=1;
-}
-if((obstaculo1.length > 0 && obstaculo1[0].distance <= 0.5) && a==1) {
-cubo3 = new THREE.Mesh( new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial());
-cubo3.position.x=4;
-escena2.add(cubo3);
+cubo4 = new THREE.Mesh( new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial());
+cubo4.position.x=-4;
+escena2.add(cubo4);
 step2=-step2;
-a=0;
 }
-if((obstaculo2.length > 0 && obstaculo2[0].distance <= 0.5) && b==0){
+
+if((obstaculo2.length > 0 && obstaculo2[0].distance <= 0.5)){
 cubo4 = new THREE.Mesh( new THREE.BoxGeometry(1,1,1), new THREE.MeshLambertMaterial());
 cubo4.position.x=-4;
 escena2.add(cubo4);
 step2=-step2;
-b=1;
-}
-if((obstaculo2.length > 0 && obstaculo2[0].distance <= 0.5) && b==1){
-cubo4 = new THREE.Mesh( new THREE.BoxGeometry(1,1,1), new THREE.MeshNormaltMaterial());
-cubo4.position.x=-4;
-escena2.add(cubo4);
+cubo3 = new THREE.Mesh( new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial());
+cubo3.position.x=4;
+escena2.add(cubo3);
 step2=-step2;
-b=0;
+a=1;
 }
+
 pelota.position.x += step2;
 
 raycaster1.set(pelota.position, new THREE.Vector3(1,0,0));
@@ -85,6 +80,5 @@ requestAnimationFrame(loop2);
 }
 var cubo3,cubo4,pelota,escena2,camara2,renderer2;
 var raycaster1,raycaster2,step2,spotlight;
-var a=0,b=0;
 setup2();
 loop2();
