f35724d67979c17df1a5eb5cbf87cfc8cb468746
UPIIDK <icontlac1000@alumno.ipn.mx>
2016-05-11 10:53:43 -0500

    Update EjemploMapa.js

--- rv/Source/JsEjercicios/MapaPerspectiva/EjemploMapa.js
+++ rv/Source/JsEjercicios/MapaPerspectiva/EjemploMapa.js
@@ -46,8 +46,8 @@ function setup(){
   
   environment.setMap(mapa);
   
-  camara=new THREE.PerspectiveCamera(45, 920 / 920, 1, 1000 );
-  //camara.position.z=30;
+  camara=new THREE.PerspectiveCamera(45, 920 / 920, 1, 1000);
+  camara.position.z=30;
   
   renderer=new THREE.WebGLRenderer();
   renderer.setSize(window.innerHeight*.95,window.innerHeight*.95);
