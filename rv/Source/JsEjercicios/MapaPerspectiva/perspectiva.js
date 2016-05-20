ba6b5d4cf665567bbcc1141d58b23d7b54bba074
UPIIDK <icontlac1000@alumno.ipn.mx>
2016-05-20 17:22:18 -0500

    Update perspectiva.js

--- rv/Source/JsEjercicios/MapaPerspectiva/perspectiva.js
+++ rv/Source/JsEjercicios/MapaPerspectiva/perspectiva.js
@@ -1,3 +1,7 @@
-
-
-
+function manejador(elEvento) {
+  var evento = elEvento || window.event;
+  var caracter = evento.charCode || evento.keyCode;
+  alert("El carácter pulsado es: " + String.fromCharCode(caracter));
+}
+ 
+document.onkeypress = manejador;
