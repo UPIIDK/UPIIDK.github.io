debef385402fadc48d80bccf7249c51d0fc0e612
UPIIDK <icontlac1000@alumno.ipn.mx>
2016-05-23 12:30:56 -0500

    Update perspectiva.js

--- rv/Source/JsEjercicios/MapaPerspectiva/perspectiva.js
+++ rv/Source/JsEjercicios/MapaPerspectiva/perspectiva.js
@@ -23,19 +23,19 @@ function muestraInformacion(elEvento) {
  environment.add(camera);
  }
  if(evento.keyCode==68){
- camara.rotation.x+=1;
+ camara.rotation.x+=Math.pi/8;
  environment.add(camera);
  }
  if(evento.keyCode==65){
- camara.rotation.x-=1;
+ camara.rotation.x-=Math.pi/8;
  environment.add(camera);
  }
  if(evento.keyCode==87){
- camara.rotation.y-=1;
+ camara.rotation.y-=Math.pi/8;
  environment.add(camera);
  }
   if(evento.keyCode==83){
- camara.rotation.y+=1;
+ camara.rotation.y+=Math.pi/8;
  environment.add(camera);
  }
   /*var mensaje = "Tipo de evento: " + evento.type + "<br>" +
