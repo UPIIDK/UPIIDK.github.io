9ee2e490c37c54523ec34fd420b9869982f7ab4f
UPIIDK <icontlac1000@alumno.ipn.mx>
2016-05-23 12:46:54 -0500

    Update EjemploMapa.js

--- rv/Source/JsEjercicios/MapaPerspectiva/EjemploMapa.js
+++ rv/Source/JsEjercicios/MapaPerspectiva/EjemploMapa.js
@@ -1,9 +1,10 @@
 
-function Wall(size,x,y){
+function Wall(size,x,y,z){
   THREE.Mesh.call(this,new THREE.BoxGeometry(size,size,size),new THREE.MeshNormalMaterial());
   this.size=size;
   this.position.x=x;
   this.position.y=y;
+  this.position.z=z;
 }
 
 Wall.prototype=new THREE.Mesh();
@@ -13,18 +14,18 @@ Environment.prototype.setMap=function(map){
   for(var i=0;i<map.length;i++)
   for(var j=0;j<map.length;j++){
     if(map[i][j] === "x")
-      this.add(new Wall(1,j-_offset,-(i-_offset)));
+      this.add(new Wall(1,j-_offset,-(i-_offset),0));
         else if(map[i][j] === "r")
     this.add(new Robot(0.5,j-_offset,-(i-_offset)));
   }
 }
 
 Environment.prototype.setMap=function(map){
-  var _offset=Math.floor(map.length/1.25);
+  var _offset=Math.floor(map.length/2);
   for(var i=0;i<map.length;i++)
   for(var j=0;j<map.length;j++){
     if(map[i][j] === "x")
-      this.add(new Wall(1,j-_offset,-(i-_offset)));
+      this.add(new Wall(1,j-_offset,-(i-_offset),-2));
         else if(map[i][j] === "r")
     this.add(new Robot(0.5,j-_offset,-(i-_offset)));
   }
