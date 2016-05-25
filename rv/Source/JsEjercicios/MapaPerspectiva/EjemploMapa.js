997eab4c56ada900e8b34199cf514333ad28d1b1
UPIIDK <icontlac1000@alumno.ipn.mx>
2016-05-25 11:48:34 -0500

    Update EjemploMapa.js

--- rv/Source/JsEjercicios/MapaPerspectiva/EjemploMapa.js
+++ rv/Source/JsEjercicios/MapaPerspectiva/EjemploMapa.js
@@ -18,7 +18,7 @@ Environment.prototype.setMap=function(map){
          if(map[i][j] === "r"){
     this.add(new Robot(0.5,j-_offset,-(i-_offset)));
     this.add(new Wall(1,j-_offset,-(i-_offset),-1.01));}
-    else
+    else if(map[i][j]!=='/0')
     this.add(new Wall(1,j-_offset,-(i-_offset),-1.01));
          
   }
