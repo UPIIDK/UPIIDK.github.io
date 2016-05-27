d49d29aa367a388f1111ea5d4c4a6dd1e3e4b614
UPIIDK <icontlac1000@alumno.ipn.mx>
2016-05-27 11:22:20 -0500

    Update Robots.js

--- rv/Source/JsEjercicios/MapaPerspectiva/Robots.js
+++ rv/Source/JsEjercicios/MapaPerspectiva/Robots.js
@@ -8,7 +8,7 @@ Sensor.prototype = new THREE.Raycaster();
 function Robot (size, x,y){
   Agent.call(this,x,y);
   this.sensor = new Sensor();
-  this.actuator = new THREE.Mesh(new THREE.SphereGeometry(size),new THREE.MeshNormalMaterial({color:'#aa0000'}));
+  this.actuator = new THREE.Mesh(new THREE.SphereGeometry(size),new THREE.MeshBasicMaterial({color:'#aa0000'}));
   this.actuator.commands=[];
   this.add(this.actuator);
 //  iNICIO
