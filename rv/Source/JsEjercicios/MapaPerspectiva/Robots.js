6ec938ac171978a9121e87b59a444784f981926d
UPIIDK <icontlac1000@alumno.ipn.mx>
2016-05-31 04:25:33 -0500

    Update Robots.js

--- rv/Source/JsEjercicios/MapaPerspectiva/Robots.js
+++ rv/Source/JsEjercicios/MapaPerspectiva/Robots.js
@@ -5,32 +5,32 @@ THREE.ImageUtils.crossOrigin=' ';
 var texturaC= THREE.ImageUtils.loadTexture('http://miguel26.github.io/rv/imagen1.jpg');
 this.Camara= new THREE.Mesh(new THREE.SphereGeometry(0.2/Tmaño) ,new THREE.MeshPhongMaterial({map: texturaC}));
 this.Camara.position.y=0.5/y;
-this.Camara.position.z=z+(0.4/Tmaño)+(1/Tmaño)+(0.5/Tmaño)+(0.5/Tmaño)+(0.25/Tmaño);
+this.Camara.position.z=z+(0.4/Tmaño)+(1/Tmaño)+(0.9/Tmaño)+(0.25/Tmaño)+(0.9/Tmaño)+(0.5/Tmaño);;
 
 var texturaA= THREE.ImageUtils.loadTexture('http://miguel26.github.io/rv/imagen2.JPG');
 this.Antena= new THREE.Mesh(new THREE.CylinderGeometry(0.15/Tmaño,0.15/Tmaño,0.4/Tmaño),new THREE.MeshPhongMaterial({map: texturaA}));
 var AA=0.4/Tmaño;
 this.Antena.position.y=0.5/y;
 this.Antena.rotation.x=Math.PI/2;
-this.Antena.position.z=z+(1/Tmaño)+(0.5/Tmaño)+(0.5/Tmaño)+(0.25/Tmaño);
+this.Antena.position.z=z+(1/Tmaño)+(0.9/Tmaño)+(0.25/Tmaño)+(0.9/Tmaño)+(0.5/Tmaño);
 
 var texturaR= THREE.ImageUtils.loadTexture('http://miguel26.github.io/rv/imagen4.jpg');
 this.Rostro= new THREE.Mesh(new THREE.BoxGeometry(0.6/Tmaño,0.6/Tmaño,0.5/Tmaño),new THREE.MeshPhongMaterial({map: texturaR}));
 this.Rostro.position.y=0.5/y;
 var AR=1/Tmaño;//Altura Rostro
-this.Rostro.position.z=z+(1/Tmaño)+(0.9/Tmaño)+(0.25/Tmaño);
+this.Rostro.position.z=z+(1/Tmaño)+(0.9/Tmaño)+(0.25/Tmaño)+(0.9/Tmaño);
 this.Rostro.rotation.x=Math.PI/2;
 
 var texturaO= THREE.ImageUtils.loadTexture('http://miguel26.github.io/rv/imagen3.jpg');
 this.OrejaI= new THREE.Mesh(new THREE.BoxGeometry(0.5/Tmaño, 0.25/Tmaño, 0.2/Tmaño ),new THREE.MeshPhongMaterial({map: texturaO}));
 this.OrejaI.position.x=((-0.6/Tmaño)+(-0.5/Tmaño))/x;
 this.OrejaI.position.y=0.5/y;
-this.OrejaI.position.z=z+(1/Tmaño)+(0.9/Tmaño)+(0.25/Tmaño);
+this.OrejaI.position.z=z+(1/Tmaño)+(0.9/Tmaño)+(0.25/Tmaño)+(0.9/Tmaño);
 
 this.OrejaD= new THREE.Mesh(new THREE.BoxGeometry( 0.5/Tmaño, 0.25/Tmaño, 0.2/Tmaño ),new THREE.MeshPhongMaterial({map: texturaO}));
 this.OrejaD.position.x=((0.6/Tmaño)+(0.5/Tmaño))/x;
 this.OrejaD.position.y=0.5/y;
-this.OrejaD.position.z=z+(1/Tmaño)+(0.9/Tmaño)+(0.25/Tmaño);
+this.OrejaD.position.z=z+(1/Tmaño)+(0.9/Tmaño)+(0.25/Tmaño)+(0.9/Tmaño);
 
 this.Cuello= new THREE.Mesh(new THREE.BoxGeometry(0.25/Tmaño,0.3/Tmaño,0.25/Tmaño),new THREE.MeshPhongMaterial({map: texturaO}));
 var AC=0.25/Tmaño;//Altura Cuello
@@ -109,11 +109,11 @@ Robot.prototype.plan=function (environment){
   this.actuator.commands=[];
   if (this.sensor.colision==true){
     this.actuator.commands.push('rotateCCW');
-    this.actuator.commands.push('Animation');
+    //this.actuator.commands.push('Animation');
 }
   else{
     this.actuator.commands.push('goStraight');
-    this.actuator.commands.push('Animation');
+    //this.actuator.commands.push('Animation');
   }
 }
 Robot.prototype.act=function(environment){
