793a825b161250b2561b2100ca3bda5ffa6c8e6d
UPIIDK <icontlac1000@alumno.ipn.mx>
2016-05-31 05:26:53 -0500

    Update Mapa.js

--- rv/Source/JsEjercicios/Laberinto/Mapa.js
+++ rv/Source/JsEjercicios/Laberinto/Mapa.js
@@ -73,7 +73,7 @@ function setup(){
   environment.setMap(mapa);
   
   camara=new THREE.PerspectiveCamera();
-  camara.position.z=(0.5/Tmaño)+(1/Tmaño)+(0.3/Tmaño);
+  camara.position.z=(0.5/1)+(1/1)+(0.3/1);
   
   var luzPuntual=new THREE.PointLight(0xFFFFFF);
 luzPuntual.position.z=camara.position.z;
