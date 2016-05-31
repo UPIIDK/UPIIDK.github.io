07653bfea0514ca16c06eeff870ea4d75350c260
UPIIDK <icontlac1000@alumno.ipn.mx>
2016-05-31 01:00:21 -0500

    Update perspectiva.js

--- rv/Source/JsEjercicios/MapaPerspectiva/perspectiva.js
+++ rv/Source/JsEjercicios/MapaPerspectiva/perspectiva.js
@@ -5,8 +5,6 @@ window.onload = function() {
                 "Tecla Z: Zoom out " + "<br>" + "Barra Espaciadora, Zoom In" + "<br>" + "Flecha, Mueven la camara" + 
                 "<br>" + "Letras AWSD, Rotan la Camara" 
   info.innerHTML += "<br>--------------------------------------<br>" + mensaje
-  //Movimiento Robot
-  Camara.rotation.y +=0.01;
 }
  
 function muestraInformacion(elEvento) {
