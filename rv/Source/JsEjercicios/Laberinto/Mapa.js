6bac17396c54be2088d4706ac6083980111c223e
UPIIDK <icontlac1000@alumno.ipn.mx>
2016-05-31 18:27:01 -0500

    Update Mapa.js

--- rv/Source/JsEjercicios/Laberinto/Mapa.js
+++ rv/Source/JsEjercicios/Laberinto/Mapa.js
@@ -61,8 +61,7 @@ Environment.prototype.setMap=function(map){
     this.add(new Techo(1,j-_offset,-(i-_offset),-0.4+(0.4/1)+(0.5/1)+(1/1)+(0.3/1)+(0.5/1)+(0.2/1)));
     }
     if(map[i][j]=="F"){
-     xl=j-_offset;;
-     yl=-(i-_offset);
+     this.add(new Meta(0.1,j-_offset,-(i-_offset),(0.4/1)+(0.5/1)+(1/1)+(0.3/1)+(0.5/1)+(0.2/1)));
   }
   }
 }
